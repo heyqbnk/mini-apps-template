813561894be8952e0be3e7c9cba0772b51e16bef
wolframdeus <wolfram.deus@gmail.com>
2020-04-18 02:12:40 +0500

    Removed component test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,12 @@
-import React, {memo, useState} from 'react';
-
-import Modal from './Modal';
-import ModalHeader from './ModalHeader';
-import ModalBody from './ModalBody';
+import React, {memo} from 'react';
 
 /**
- * Компонент, с которого начинается весь визуал и логика по отображению
- * необходимых элементов.
+ * Visual entry of application
  * @type {React.NamedExoticComponent<object>}
  */
 const App = memo(() => {
-  const [showModal, setShowModal] = useState(true);
-
   return (
-    <div>
-      <div onClick={() => setShowModal(true)}>Open</div>
-      <Modal
-        show={showModal}
-        onClose={() => setShowModal(false)}
-      >
-        <ModalHeader>Ошибка</ModalHeader>
-        <ModalBody>
-          <div style={{height: 5000}}>
-            Какая-то ошибка ебана
-          </div>
-        </ModalBody>
-      </Modal>
-    </div>
+    <div>Hello VK Mini Apps!</div>
   );
 });
 
